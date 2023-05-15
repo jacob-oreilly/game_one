0c45514164b7e9529bf59cad6500965f4128b374
jacob-oreilly <jakeor45@gmail.com>
2023-05-14 22:22:44 -0700

    adding in bevy_prototype_lyon for drawing shapes and added ina rectangle as barrier. Need to fix positioning

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
 use bevy::prelude::*;
+use bevy_prototype_lyon::prelude::*;
 
 fn main() {
     App::new()
+        .insert_resource(Msaa::Sample4)
         .add_plugins(DefaultPlugins)
+        .add_plugin(ShapePlugin)
+        .add_startup_system(setup_system)
         .run();
+}
+
+fn setup_system(mut commands: Commands) {
+    let shape = shapes::Rectangle {
+        extents: Vec2 { x: 800.0, y: 600.0 },
+        origin: RectangleOrigin::TopLeft
+    };
+
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        ShapeBundle {
+            path: GeometryBuilder::build_as(&shape),
+            ..default()
+        },
+        Fill::color(Color::CYAN),
+        Stroke::new(Color::BLACK, 10.0),
+    ));
 }
